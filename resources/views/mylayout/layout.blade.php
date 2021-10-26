0c3dcd9de991eb7be41f447c7feed80dc92a01b3
Faishol Shinryuhachi <fahmifaishol11@gmail.com>
2021-10-26 22:18:50 +0700

    Ubah Icons

--- resources/views/mylayout/layout.blade.php
+++ resources/views/mylayout/layout.blade.php
@@ -14,8 +14,8 @@ The above copyright notice and this permission notice shall be included in all c
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material/assets/img/apple-icon.png')}}">
-  <link rel="icon" type="image/png" href="{{ asset('material/assets/img/favicon.png')}}">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('material/assets/img/kgb.png')}}">
+  <link rel="icon" type="image/png" href="{{ asset('material/assets/img/kgb.png')}}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     E-KGB
